c3c2ba70205c969956f8bdfd5a87d573f21e8488
Idan Melamed <idanmel@gmail.com>
2021-02-16 12:57:23 +0200

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gp-birthday-greeting
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gp_birthday_greeting/core_test.clj
@@ -0,0 +1,7 @@
+(ns gp-birthday-greeting.core-test
+  (:require [clojure.test :refer :all]
+            [gp-birthday-greeting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
