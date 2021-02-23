4f8b282890657ba2d77c30905d4068f7b41b28d1
Pradeep Bishnoi <pradeepbishnoi@gmail.com>
2021-02-23 18:32:41 +0530

    Squashed commit of the following:
    commit bb9149bc8f403e0628003f5764b8bc0b7ab9cd16
    Author: Pradeep Bishnoi <pradeepbishnoi@gmail.com>
    Date:   Tue Feb 23 18:32:14 2021 +0530
    
        mob next [ci-skip]
    
    commit ea00d59da25f7316741af8f9af45aa66688b5d10
    Author: Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
    Date:   Tue Feb 23 13:41:07 2021 +0100
    
        mob next [ci-skip]
    
    commit 2ce9127ffaecfbaa7df0dd22217d844de5c623de
    Author: Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
    Date:   Tue Feb 23 13:26:10 2021 +0100
    
        Remove .lsp/sqlite.db
    
    commit 5593958d2c0e49abf223f2bf8da205120db55133
    Author: Pradeep Bishnoi <pradeepbishnoi@gmail.com>
    Date:   Tue Feb 23 17:54:07 2021 +0530
    
        mob next [ci-skip]

--- src/gp_birthday_greeting/core.clj
+++ src/gp_birthday_greeting/core.clj
@@ -1,6 +1,7 @@
 (ns gp-birthday-greeting.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -15,10 +16,29 @@
   (str/split friend #"\s*,\s*"))
 
 
+(let [{a :b} {:b 1}]
+  [a])
+
+
+(defn birthdee->reminder-date [birthdee]
+  (let [{:keys [:month :day]} birthdee]
+    {:month month :day (dec day)}))
+
+(defn template [] "I am template")
+
+(defn reminder? [birthdee date]
+  (let [{birthdee-month :month
+         birthdee-day   :day} birthdee
+        {given-month :month
+         given-day   :day}    date]
+    (and (= birthdee-month given-month)
+         (= birthdee-day given-day))))
+
 (defn parse-dob [dob]
-  {:year  year
-   :month month
-   :day   day})
+  (let [[year month day] (str/split dob #"/")]
+    {:year  (Integer/parseInt year)
+     :month (Integer/parseInt month)
+     :day   (Integer/parseInt day)}))
 
 (defn parse-line [friend]
   (let [[firstname lastname dob email] (split-line friend)
@@ -31,8 +51,19 @@
      :day       day
      :email     email}))
 
+(defn parse-file [file] (->>
+                         file
+                         (slurp)
+                         (str/split-lines)
+                         (map parse-line)))
 
 (defn matching-birthdee? [friend day])
+
+;; friends == path to the file
+;; today == today's date
+(defn send-emails! [friends today template]
+  (let [db (parse-file friends)]))
+;; TODO: We want to begin from here on next session
 
 
 (comment
@@ -41,5 +72,8 @@
   (parse-line friend)
 
   (str/split "one, two, three" #"\s*,\s*")
-
+  (parse-dob "2000/01/")
+  (reminder? (parse-line friend) {:month 1 :day 19})
+  (parse-file (io/resource "friends.txt"))
+  
   ,)
